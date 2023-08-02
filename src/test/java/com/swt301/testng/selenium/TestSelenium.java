d0a01ab997a403ecfba3a8b70b3a52dec2665ae0
trichoip <107303342+trichoip@users.noreply.github.com>
2023-08-02 21:34:14 +0700

    first commit

---
+++ src/test/java/com/swt301/testng/selenium/TestSelenium.java
@@ -0,0 +1,23 @@
+package com.swt301.testng.selenium;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestSelenium {
+
+    @Test
+    public void main() throws InterruptedException {
+        // WebDriverManager.chromedriver().setup();
+        // WebDriver driver = new ChromeDriver();
+        // driver.get("http://localhost:8080/");
+        // driver.findElement(By.xpath("/html[1]/body[1]/form[1]/input[1]")).sendKeys("admin");
+        // driver.findElement(By.xpath("/html[1]/body[1]/form[1]/input[2]")).sendKeys("1");
+        // driver.findElement(By.xpath("/html[1]/body[1]/form[1]/input[3]")).click();
+        // Thread.sleep(3000);
+        // driver.quit();
+
+    }
+}
